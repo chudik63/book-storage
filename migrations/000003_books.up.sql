cbbed141643401990dd8f69a5982302bbf0f2e40
Vitalya <vitamin11182005@gmail.com>
2024-12-10 00:59:48 +0300

    edit mistake in sql

--- migrations/000003_books.up.sql
+++ migrations/000003_books.up.sql
@@ -5,5 +5,5 @@ CREATE TABLE IF NOT EXISTS books (
     title VARCHAR(255) NOT NULL,
     author VARCHAR(255),
     comment TEXT,
-    CONSTRAINT fk_storage FOREIGN KEY(storage_id) REFERENCES storage(id)
+    CONSTRAINT fk_storage FOREIGN KEY(storage_id) REFERENCES storages(id)
 );
