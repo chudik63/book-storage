4324dba85226944cf578837779e720a7f904bdbf
Vitalya <vitamin11182005@gmail.com>
2025-03-05 16:14:01 +0300

    add errors

--- backend/internal/models/errors.go
+++ backend/internal/models/errors.go
@@ -5,6 +5,11 @@ import "errors"
 var (
 	ErrLoginAlreadyExists     = errors.New("such login already exists")
 	ErrEmailAlreadyRegistered = errors.New("such email already registered")
+	ErrNotFound               = errors.New("nothing was found")
+	ErrPasswordIsTooLong      = errors.New("password must be shorter than 12")
+	ErrLoginIsTooLong         = errors.New("login must be shorter than 12")
+	ErrNameIsTooLong          = errors.New("name must be shorter than 20")
+	ErrEmailFormat            = errors.New("wrong email format")
 )
 
 // сагя ижи гаъуй
