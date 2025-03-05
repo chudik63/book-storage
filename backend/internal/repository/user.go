a6bc4ef33ec5a10c7beaff9dc0604d9331ba900a
Vitalya <vitamin11182005@gmail.com>
2025-03-05 16:15:47 +0300

    edit repo methods

--- backend/internal/repository/user.go
+++ backend/internal/repository/user.go
@@ -4,6 +4,9 @@ import (
 	"book-storage/internal/database/postgres"
 	"book-storage/internal/models"
 	"context"
+	"database/sql"
+	"errors"
+	"fmt"
 	"strconv"
 
 	sq "github.com/Masterminds/squirrel"
@@ -21,30 +24,40 @@ func NewUserRepository(db postgres.DB) *UserRepository {
 
 func (r *UserRepository) Create(ctx context.Context, user *models.User) error {
 	_, err := sq.Insert("users").
-		Columns("login", "name", "password", "email").
-		Values(user.Name, user.Login, user.Password, user.Email).
+		Columns("login", "name", "email", "password_hash", "password_salt", "created_at").
+		Values(user.Login, user.Name, user.Email, user.Password, user.Salt, user.CreatedAt).
 		PlaceholderFormat(sq.Dollar).
 		RunWith(r.db).
 		Exec()
 
-	return err
+	if err != nil {
+		return fmt.Errorf("failed to create user: %w", err)
+	}
+
+	return nil
 }
 
 func (r *UserRepository) GetByCredentials(ctx context.Context, equations Creds) (*models.User, error) {
 	var user models.User
 
-	rows, err := sq.Select("id, login, name, password, email").
-		From("users").
-		Where(equations).
-		PlaceholderFormat(sq.Dollar).
-		RunWith(r.db).
-		Query()
+	sb := sq.Select("id, login, name, password_hash, email").
+		From("users")
 
-	if err != nil {
-		return nil, err
+	for field, value := range equations {
+		sb = sb.Where(sq.Eq{field: value})
 	}
 
-	rows.Scan(&user.ID, &user.Login, &user.Name, &user.Password, &user.Email)
+	row := sb.PlaceholderFormat(sq.Dollar).
+		RunWith(r.db).
+		QueryRow()
+
+	err := row.Scan(&user.ID, &user.Login, &user.Name, &user.Password, &user.Email)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, models.ErrNotFound
+		}
+		return nil, fmt.Errorf("failed to get by credential: %w", err)
+	}
 
 	return &user, nil
 }
